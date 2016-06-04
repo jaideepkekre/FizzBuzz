7d24e98c3f51b09221acbf776b2ce1eb0098c004
jaideepkekre <jaideepkekre@gmail.com>
2016-06-04 22:08:43 +0530

    added code to dis-assemble the function to study internal instruction 180 vs 109 for Lazy vs smart !

--- FizzBuzz_Lazy.py
+++ FizzBuzz_Lazy.py
@@ -1,19 +1,26 @@
-x=range(1,101,1)
-three=False
-five=False
-for number in x :
-	flag=-1
-	T_print=number
-	if number%3 == 0 :
-		three = True
-	if number% 5 == 0 :
-		five = True
-	if three and five :
-		T_print="FizzBuzz"
-	elif five:
-		T_print = "Buzz"
-	elif three:
-		T_print="Fizz"	
-	three = False
-	five = False
-	print T_print
+def test():
+	x=range(1,101,1)
+	three=False
+	five=False
+	for number in x :
+		flag=-1
+		T_print=number
+		if number%3 == 0 :
+			three = True
+		if number% 5 == 0 :
+			five = True
+		if three and five :
+			T_print="FizzBuzz"
+		elif five:
+			T_print = "Buzz"
+		elif three:
+			T_print="Fizz"	
+		three = False
+		five = False
+		print T_print
+
+import dis 
+test()
+#dis-assembles the code to see its instructions , 180 lines in this case :/
+print "****************"
+dis.dis(test)
--- FizzBuzz_Smart.py
+++ FizzBuzz_Smart.py
@@ -3,13 +3,21 @@
 """
 author: Jaideep Kekre
 """
+import dis
 
-for number in range(1,101,1):
-	if number%15 == 0 : 
-		print "FizzBuzz"
-	elif number%5 == 0 :
-		print "Buzz"
-	elif number%3==0:
-		print"Fizz"
-	else:
-		print number
+def test():
+	for number in range(1,101,1):
+		if number%15 == 0 : 
+			print "FizzBuzz"
+		elif number%5 == 0 :
+			print "Buzz"
+		elif number%3==0:
+			print"Fizz"
+		else:
+			print number
+
+
+test()
+print"**************"
+#dis-assemble code toi see instructions, 109 in this case
+dis.dis(test)
